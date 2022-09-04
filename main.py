61540371e996b0f88a28f2aca7102708134b2e50
Pham Minh Tien <phamminhtien2405@gmail.com>
2022-09-05 03:28:43 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from turtle import *
+import colorsys
+speed(0)
+hideturtle()
+bgcolor('black')
+tracer(6)
+width(2)
+h = 0.001
+for i in range(1000):
+    color(colorsys.hsv_to_rgb(h,1,1))
+    forward(100)
+    left(60)
+    forward(100)
+    right(120)
+    circle(50)
+    left(240)
+    forward(100)
+    left(60)
+    forward(100)
+    h += 0.02
+    color(colorsys.hsv_to_rgb(h,1,1))
+    forward(100)
+    right(60)
+    forward(100)
+    left(120)
+    circle(-50)
+    right(240)
+    forward(100)
+    left(2)
+    h += 0.02
+done()
